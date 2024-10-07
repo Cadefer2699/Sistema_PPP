b83176056f6619f157bf8b88e9fb4d2aa2f8a2a2
faviocpz <103940536+faviocpz@users.noreply.github.com>
2024-10-07 08:59:52 -0500

    Ya

--- bd.py
+++ bd.py
@@ -1,11 +1,24 @@
 import pymysql
 
-#Desarrollo
 def obtener_conexion():
-    return pymysql.connect(host='roundhouse.proxy.rlwy.net',
-                                port=54363,
-                                user='root',
-                                password='UAumJRVIMWnTGqRdkZNDqvocCpHjzHKl',
-                                db='db_calidad')
+    try:
+        conexion = pymysql.connect(
+            host='junction.proxy.rlwy.net',
+            port=39981,
+            user='root',
+            password='pCWZPaxqsNKSYInhPHeyFknIWiRFpSpb',
+            db='bd_ppp'
+        )
+        print("Conexión exitosa a la base de datos.")
+        return conexion
+    
+    except Exception as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
 
+conexion = obtener_conexion()
 
+if conexion:
+    conexion.close()
+else:
+    print("No se pudo establecer conexión con la base de datos.")
--- routers/router_main.py
+++ routers/router_main.py
@@ -1,10 +1,76 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, jsonify, make_response, session
 
-# Crea el Blueprint
+import hashlib
+import random
+import os
+from werkzeug.utils import secure_filename
+from bd import obtener_conexion
+from bd import obtener_conexion
+import controladores.controlador_usuario as controlador_usuario
+
+import time
+
+login_attempts = {}
 router_main = Blueprint('router_main', __name__)
 
-# Define las rutas en el blueprint
 @router_main.route("/")
 @router_main.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
+
+@router_main.route("/procesar_login", methods=["POST"])
+def procesar_login():
+    try:
+        username = request.json.get('username')
+        password = request.json.get('password')
+        #usuario = controlador_usuario.obtener_usuario_con_tipopersona_por_username(username)
+        usuario = username
+
+        # Inicializar el diccionario de intentos para el usuario si no existe
+        if username not in login_attempts:
+            login_attempts[username] = {'attempts': 0, 'last_attempt_time': 0}
+        """
+        # Verificar si el usuario está bloqueado
+        if login_attempts[username]['attempts'] >= 3 and (time.time() - login_attempts[username]['last_attempt_time']) < 300:
+            return jsonify({'mensaje': 'Cuenta bloqueada. Intente de nuevo más tarde.', 'logeo': False})
+        
+        if usuario == None:
+            return jsonify({'mensaje':'El usuario no existe', 'logeo':False})
+        
+        elif username != usuario[1]:
+            return jsonify({'mensaje':'El username es incorrecto', 'logeo':False})
+        
+        elif not usuario[3]:
+            return jsonify({'mensaje':'El usuario está inactivo', 'logeo':False})
+        """
+        #else:
+        # Encriptar password ingresado por usuario
+        h = hashlib.new("sha256")
+        h.update(bytes(password, encoding="utf-8"))
+        print(password)
+        encpassword = h.hexdigest()
+        if encpassword == usuario[4]:
+            # Obteniendo token
+            t = hashlib.new("sha256")
+            entale = random.randint(1, 1024)
+            strEntale = str(entale)
+            t.update(bytes(strEntale, encoding="utf-8"))
+            token = t.hexdigest()
+            controlador_usuario.actualizar_token(username, token)
+
+            """
+            persona = controlador_persona.obtener_persona_por_id(usuario[4])
+            foto = persona[9]
+            nombre = persona[1].split()[0]
+            """
+            
+            # Almacenar el ID del usuario en la sesión
+            session['user_id'] = usuario[0]
+            return jsonify({'logeo': True, 'token': token})
+            #return jsonify({'logeo': True, 'token': token, 'foto':foto, 'nombre':nombre})
+        else:
+            login_attempts[username]['attempts'] += 1
+            login_attempts[username]['last_attempt_time'] = time.time()
+            return jsonify({'mensaje': 'La contraseña es incorrecta', 'logeo': False})
+    except NameError:
+        return jsonify({'mensaje':'Error al procesar el login'+NameError, 'logeo':False})
