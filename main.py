cc0f0db0b9574700d986c636dadb502d1e8c729e
wilder315 <byjawi315@gmail.com>
2024-10-10 09:27:26 -0500

    ya funciona estudiante

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routers.router_main import router_main  # Importa el Blueprint
 from routers.router_facultad import router_facultad
+from routers.router_estudiante import router_estudiante
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.secret_key = 'super-secret'
 # Registra el Blueprint
 app.register_blueprint(router_main)
 app.register_blueprint(router_facultad)
+app.register_blueprint(router_estudiante)
 
 
 if __name__ == "__main__":
