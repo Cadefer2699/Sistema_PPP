da70d2235668dfedfb263c66d28db3fad37a3029
faviocpz <103940536+faviocpz@users.noreply.github.com>
2024-11-05 03:01:04 -0500

    CRUDS (3)

--- routers/router_main.py
+++ routers/router_main.py
@@ -13,22 +13,12 @@ import time
 login_attempts = {}
 router_main = Blueprint('router_main', __name__)
 
+# Login
+
 @router_main.route("/")
 @router_main.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("/dashboard/login.html")
-
-@router_main.route("/index")
-def index():
-    return render_template("/dashboard/index.html")
-
-@router_main.route("/indexga")
-def gestion_academica():
-    return render_template("/gestion_academica/index.html")
-
-@router_main.route("/indexppp")
-def practicas_pre_profesionales():
-    return render_template("/ppp/index.html")
 
 @router_main.route("/procesar_login", methods=["POST"])
 def procesar_login():
@@ -43,40 +33,26 @@ def procesar_login():
         username = request.json.get('username')
         password = request.json.get('password')
         usuario = controlador_usuario.obtener_usuario_con_tipopersona_por_username(username)
-
-        # Inicializar el diccionario de intentos para el usuario si no existe
         if username not in login_attempts:
             login_attempts[username] = {'attempts': 0, 'last_attempt_time': 0}
-
-        # Verificar si el usuario está bloqueado (desbloquear después de 5 minutos)
         if login_attempts[username]['attempts'] >= 3 and (time.time() - login_attempts[username]['last_attempt_time']) < 300:
-            return jsonify({'mensaje': 'Cuenta bloqueada. Intente de nuevo más tarde.', 'logeo': False})
-        
+            return jsonify({'mensaje': 'Cuenta bloqueada. Intente de nuevo más tarde.', 'logeo': False})      
         if usuario is None:
             return jsonify({'mensaje': 'El usuario no existe', 'logeo': False})
-
         elif usuario[2] == "I":
             return jsonify({'mensaje': 'El usuario está inactivo', 'logeo': False})
-
         else:
             h = hashlib.new("sha256")
             h.update(bytes(password, encoding="utf-8"))
             encpassword = h.hexdigest()
 
             if encpassword == usuario[3]:
-                # Resetear los intentos de login fallidos tras un login exitoso
                 login_attempts[username] = {'attempts': 0, 'last_attempt_time': 0}
-
-                # Obtener datos del usuario para almacenar en la sesión
                 persona = controlador_usuario.obtener_datos_usuario(usuario[0])
                 nombre = persona[0].split()[0]
                 apellido = persona[1].split()[0]
                 foto = persona[2]
-
-                # Almacenar el ID del usuario en la sesión
                 session['user_id'] = usuario[0]
-
-                # Retornar los datos de sesión y confirmar el login exitoso
                 return jsonify({
                     'logeo': True,
                     'nombre': nombre,
@@ -84,52 +60,73 @@ def procesar_login():
                     'foto': foto
                 })
             else:
-                # Aumentar el número de intentos fallidos
                 login_attempts[username]['attempts'] += 1
                 login_attempts[username]['last_attempt_time'] = time.time()
                 return jsonify({'mensaje': 'La contraseña es incorrecta', 'logeo': False})
     except Exception as e:
         return jsonify({'mensaje': f'Error al procesar el login: {str(e)}', 'logeo': False})
-    
+
+# Principal
+
+@router_main.route("/index")
+def index():
+    return render_template("/dashboard/index.html")
+
+@router_main.route("/indexga")
+def gestion_academica():
+    return render_template("/gestion_academica/index.html")
+
+@router_main.route("/indexppp")
+def practicas_pre_profesionales():
+    return render_template("/ppp/index.html")
+
 @router_main.route('/home')
 def home():
     return render_template('home.html')
-
-@router_main.route('/estudiante')
-def estudiante():
-    return render_template('/gestion_academica/estudiante.html')
-
-@router_main.route('/ppp_registro')
-def ppp_registro():
-    return render_template('ppp_registro.html')
 
 @router_main.route('/perfil')
 def perfil():
     return render_template('perfil.html')
 
+# Módulo de Gestión Académica
+
 @router_main.route("/docente")
 def docente():
     return render_template('/gestion_academica/docente.html')  
-
-@router_main.route("/dap")
-def dap():
-    return render_template('/gestion_academica/dap.html')  
-
-@router_main.route("/facultad")
-def facultad():
-    return render_template('gestion_academica/facultad.html') 
 
 @router_main.route("/escuela")
 def escuela():
     return render_template('/gestion_academica/escuela.html')
 
-@router_main.route("/supervision") 
-def supervision(): 
-    return render_template('gestion_academica/supervision.html')
+@router_main.route('/estudiante')
+def estudiante():
+    return render_template('/gestion_academica/estudiante.html')
 
-@router_main.route("/tipopracticas")
-def tipopracticas(): 
-    return render_template('gestion_academica/tipoPracticas.html')
+@router_main.route("/facultad")
+def facultad():
+    return render_template('gestion_academica/facultad.html') 
+
+@router_main.route("/genero")
+def genero():
+    return render_template('gestion_academica/genero.html') 
+
+@router_main.route("/institucion")
+def institucion():
+    return render_template('gestion_academica/institucion.html') 
+
+@router_main.route("/semestre")
+def semestre():
+    return render_template('gestion_academica/semestre.html')
+
+@router_main.route("/usuario")
+def usuario():
+    return render_template('gestion_academica/usuario.html')
+
+# Módulo de Prácticas Pre Profesionales
+
+@router_main.route('/ppp_registro')
+def ppp_registro():
+    return render_template('ppp_registro.html')
 
 @router_main.route("/InformeInicialEstudiante")
 def informeInicialEstudiante():
